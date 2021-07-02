a65c6c6f630ec7ad286b3e0c7c8df9a599ece586
Sasha Karachev <shuricans@gmail.com>
2021-07-02 16:01:24 +0300

    homework 2.6

---
+++ src/main/java/ru/gb/karachev/homework_2/lesson_6/Server.java
@@ -0,0 +1,94 @@
+package ru.gb.karachev.homework_2.lesson_6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Scanner scanner;
+
+    public static void main(String[] args) {
+        new Server(6543);
+    }
+
+    public Server(int port) {
+        startServer(port);
+    }
+
+    private void startServer(int port) {
+        ExecutorService executor = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Wait connection...");
+            socket = serverSocket.accept();
+            System.out.println("Client connected!");
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            executor = Executors.newSingleThreadExecutor();
+            executor.submit(new Reader(in));
+
+            scanner = new Scanner(System.in);
+
+            while (true) {
+                String msg = scanner.nextLine();
+                if("/end".equalsIgnoreCase(msg)) {
+                    break;
+                }
+                out.writeUTF(msg);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(executor != null) {
+                executor.shutdown();
+            }
+            closeConnections();
+        }
+    }
+
+    private void closeConnections() {
+        try {
+            in.close();
+            out.close();
+            serverSocket.close();
+            socket.close();
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class Reader implements Runnable {
+
+        private final DataInputStream in;
+
+        public Reader(DataInputStream in) {
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    String msg = in.readUTF();
+                    System.out.println("client: " + msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
