a9924c2dd9542d14f0c9dc41d1f2df784f4b5176
Sasha Karachev <shuricans@gmail.com>
2021-09-12 15:04:11 +0300

    homework 3.5 init

---
+++ src/main/java/ru/gb/karachev/homework_3/lesson_5/Race.java
@@ -0,0 +1,12 @@
+package ru.gb.karachev.homework_3.lesson_5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
